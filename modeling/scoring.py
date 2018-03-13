4e4b41678ed6fe6af3be09b2c99a3952483fb251
andreanr <andrea.navarrete126@gmail.com>
2018-03-13 12:31:53 -0600

    add evaluations for changes as well

--- modeling/scoring.py
+++ modeling/scoring.py
@@ -11,6 +11,27 @@ def generate_binary_at_x(test_predictions, cutoff):
     test_predictions_binary = [1 if x >= cutoff else 0 for x in test_predictions]
     return test_predictions_binary
 
+def confusion_matrix_cost_at_x(test_labels, test_prediction_binary_at_x, test_costs):
+    """
+    Returns the raw number of a given metric:
+        'TP_c' = true positives,
+        'TN_c' = true negatives,
+        'FP_c' = false positives,
+        'FN_c' = false negatives
+    """
+    urban = test_costs[0]
+    true_positive = [1 if x == 1 and y == 1 and z == 1 else 0 for (x, y, z) in zip(test_prediction_binary_at_x, test_labels, urban)]
+    false_positive = [1 if x == 1 and y == 0 and z == 1 else 0 for (x, y, z) in zip(test_prediction_binary_at_x, test_labels, urban)]
+    true_negative = [1 if x == 0 and y == 0 and z == 1 else 0 for (x, y, z) in zip(test_prediction_binary_at_x, test_labels, urban)]
+    false_negative = [1 if x == 0 and y == 1 and z == 1 else 0 for (x, y, z) in zip(test_prediction_binary_at_x, test_labels, urban)]
+
+    TP_c = np.sum(true_positive)
+    TN_c = np.sum(true_negative)
+    FP_c = np.sum(false_positive)
+    FN_c = np.sum(false_negative)
+
+    return TP_c, TN_c, FP_c, FN_c
+
 
 def confusion_matrix_at_x(test_labels, test_prediction_binary_at_x):
     """
@@ -58,6 +79,7 @@ def calculate_all_evaluation_metrics(test_label, test_predictions, test_costs):
         test_predictions_binary_at_x = generate_binary_at_x(test_predictions, cutoff)
         # confusion matrix
         TP, TN, FP, FN = confusion_matrix_at_x(test_label,  test_predictions_binary_at_x)
+
         all_metrics["true positives@|{}".format(str(cutoff))] = TP
         all_metrics["true negatives@|{}".format(str(cutoff))] = TN
         all_metrics["false positives@|{}".format(str(cutoff))] = FP
@@ -73,6 +95,18 @@ def calculate_all_evaluation_metrics(test_label, test_predictions, test_costs):
         # cost sensity
         all_metrics["savings@|{}".format(str(cutoff))] = savings_score(test_label, test_predictions_binary_at_x, test_costs)
         all_metrics["cost_loss@|{}".format(str(cutoff))] = cost_loss(test_label, test_predictions_binary_at_x, test_costs)
+
+        #Adding only the changes
+        TP_c, TN_c, FP_c, FN_c = confusion_matrix_cost_at_x(test_label, test_predictions_binary_at_x, test_costs)
+        all_metrics["true positives ch@|{}".format(str(cutoff))] = TP_c
+        all_metrics["true negatives ch@|{}".format(str(cutoff))] = TN_c
+        all_metrics["false positives ch@|{}".format(str(cutoff))] = FP_c
+        all_metrics["false negatives ch@|{}".format(str(cutoff))] = FN_c
+        all_metrics["precision ch@|{}".format(str(cutoff))] = [TP_c / ((TP_c + FP_c) * 1.0) if (TP_c + FP_c) > 0 else 'Null'][0]
+        all_metrics["recall@|{}".format(str(cutoff))] = [TP_c / ((TP_c + FN_c) * 1.0) if (TP_c + FN_c)> 0 else 'Null'][0]
+        all_metrics["f1 ch@|{}".format(str(cutoff))] = [(2* TP_c) / ((2*TP_c + FP_c + FN_c)*1.0) if (TP_c + FP_c + FN_c) > 0 else 'Null'][0]
+        all_metrics["auc ch@|{}".format(str(cutoff))] = (TP_c + TN_c) / ((TP_c + TN_c + FP_c + FN_c)*1.0)
+
     return all_metrics
 
 def cv_evaluation_metrics(fold_metrics):
